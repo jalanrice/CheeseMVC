741f4e7afe04af3a71204a70a7192ed0e65aa643
Jeffrey Rice <jalanrice@gmail.com>
2017-03-23 14:44:51 -0500

    after class 8 video, before exercises

--- src/CheeseMVC/Models/Cheese.cs
+++ src/CheeseMVC/Models/Cheese.cs
@@ -9,5 +9,15 @@ namespace CheeseMVC.Models
     {
         public string Name { get; set; }
         public string Description { get; set; }
+        public int CheeseId { get; set; }
+        private static int nextId = 1;
+
+        public Cheese()
+        {
+            CheeseId = nextId;
+            nextId++;
+        }
+
+    
     }
 }
